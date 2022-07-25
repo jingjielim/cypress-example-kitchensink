ca6d87f3ffa5cb615841e42533a49b7623674fb2
jingjielim <jingjie3000@gmail.com>
2022-07-25 00:57:58 -0400

    Add first fixture example

--- cypress/e2e/2-advanced-examples/myFirstTest.cy.js
+++ cypress/e2e/2-advanced-examples/myFirstTest.cy.js
@@ -1,32 +1,37 @@
 // const navbarText = Cypress.env('navbarText')
 
 context('My First Test', () => {
-  before(() => {
-    cy.request('https://api.spacexdata.com/v3/missions')
-      .its('body')
-      .should('have.length', 10)
+  it('pulls data from a fixture', ()=>{
+    cy.fixture('example').then((data)=>{
+      cy.log('DATA: ', data)
+    })
   })
+  // before(() => {
+  //   cy.request('https://api.spacexdata.com/v3/missions')
+  //     .its('body')
+  //     .should('have.length', 10)
+  // })
 
-  beforeEach(() => {
-    cy.visit('/')
-  })
+  // beforeEach(() => {
+  //   cy.visit('/')
+  // })
 
-  afterEach(() => {
-    cy.log('after each hook is here')
-  })
+  // afterEach(() => {
+  //   cy.log('after each hook is here')
+  // })
 
-  after(() => {
-    cy.log('the final after hook runs once')
-  })
+  // after(() => {
+  //   cy.log('the final after hook runs once')
+  // })
 
-  it('has an h1 on the page', () => {
-    cy.visit('/')
-    cy.get('h1').should('exist')
-  })
+  // it('has an h1 on the page', () => {
+  //   cy.visit('/')
+  //   cy.get('h1').should('exist')
+  // })
 
-  it('renders the correct h1 text', () => {
-    cy.get('h1').should('contain.text', 'Kitchen Sink')
-  })
+  // it('renders the correct h1 text', () => {
+  //   cy.get('h1').should('contain.text', 'Kitchen Sink')
+  // })
 
   // it('has an h1 on the page', () => {
   //   cy.get('h1').should('exist')
