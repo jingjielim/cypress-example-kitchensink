5e30dcd563bd7f66f18b899e77648f6787282a07
jingjielim <jingjie3000@gmail.com>
2022-07-26 00:24:01 -0400

    Setting up cypress fixtures

--- cypress/e2e/2-advanced-examples/myFirstTest.cy.js
+++ cypress/e2e/2-advanced-examples/myFirstTest.cy.js
@@ -1,9 +1,32 @@
 // const navbarText = Cypress.env('navbarText')
 
 context('My First Test', () => {
-  it('pulls data from a fixture', ()=>{
-    cy.fixture('example').then((data)=>{
+  beforeEach(() => {
+    cy.fixture('example').then(function (data) {
+      this.data = data
+      cy.log('THIS: ', this.data)
+    })
+  })
+
+  it('uses fixture data in a network request', function () {
+    cy.visit('/commands/network-requests')
+    cy.intercept('GET', '**/comments/*', this.data).as('getComment')
+    cy.get('.network-btn').click()
+    cy.wait('@getComment').then((res) => {
+      cy.log('Response: ', res)
+    })
+  })
+
+  it('pulls data from a fixture', () => {
+    cy.fixture('example').then((data) => {
       cy.log('DATA: ', data)
+    })
+  })
+
+  it('updates fixture data inline', () => {
+    cy.fixture('example').then((data) => {
+      data.email = 'updated@mail.com'
+      cy.log('UPDATED: ', data)
     })
   })
   // before(() => {
